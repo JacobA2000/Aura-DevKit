d3645af0b05f093a503e06893e2bb8231c2e839c
Jacob Allen <JacobAllen785@gmail.com>
2020-12-20 13:39:38 +0000

    Main File Dirctory Location Fixes

--- GeneralHandlers/FileHandler.py
+++ GeneralHandlers/FileHandler.py
@@ -1,3 +1,8 @@
 import os
 
-mainDirPath = os.path.abspath(f"..")
+mainDirPath = ""
+
+if os.name == "nt":
+    mainDirPath = os.path.split(os.path.dirname(os.path.abspath(__file__)))[0]
+else:
+    mainDirPath = os.path.dirname(os.path.abspath(__file__))
